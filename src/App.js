48068d86ac27e4a00ec339d93b1bcbf976984265
Arnaud <bandonkeyea@gmail.com>
2022-07-04 13:29:34 -0600

    Use react router to display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Countries from './components/Countries/Countries';
+// import Map from './components/Map/Map';
 
 const App = () => (
-    <div>App</div>
-)
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<Countries />} />
+    </Routes>
+  </div>
+);
 
 export default App;
