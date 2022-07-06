f58503fb25b765f59ad5829c0771270d87095487
Arnaud <bandonkeyea@gmail.com>
2022-07-06 10:10:13 -0600

    Create Details and the details of country data

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import {
+  Detail, FlexDetails, TitleCountry, TextPop, LeftRight, TitleDetails, Text,
+} from './details.styled';
+
+const Details = () => {
+  const countries = useSelector((state) => state.reduceData);
+  const { country } = useParams();
+  console.log('con', country);
+  const filterCountries = countries.filter((country) => country.continent === 'Europe');
+  const countryName = filterCountries.filter((item) => item.name === country);
+  console.log('haha', countryName);
+  return (
+    <div className="details">
+      <Header />
+      {countryName.map((country, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <Detail key={index} className="detail">
+          <TitleCountry>
+            {country.name}
+          </TitleCountry>
+          <img src={country.flag} alt={country.name} width={250} />
+          <TextPop>
+            Population:
+            {' '}
+            {country.population}
+          </TextPop>
+          <FlexDetails>
+            <LeftRight>
+              <TitleDetails>
+                Covid-19 data
+              </TitleDetails>
+              <Text>
+                tests:
+                {' '}
+                {country.tests}
+              </Text>
+              <Text>
+                cases:
+                {' '}
+                {country.cases}
+              </Text>
+              <Text>
+                Deaths:
+                {' '}
+                {country.death}
+              </Text>
+              <Text>
+                Recovered:
+                {' '}
+                {country.recovered}
+              </Text>
+            </LeftRight>
+            <LeftRight>
+              <TitleDetails>
+                Today&apos;s updates:
+              </TitleDetails>
+              <Text>
+                Today&apos;s cases:
+                {' '}
+                {country.todayCases}
+              </Text>
+              <Text>
+                Today&apos;s deaths:
+                {' '}
+                {country.todayDeath}
+              </Text>
+              <Text>
+                Today&apos;s recovered:
+                {' '}
+                {country.todayRecovered}
+              </Text>
+            </LeftRight>
+          </FlexDetails>
+        </Detail>
+      ))}
+      <Footer />
+    </div>
+
+  );
+};
+
+export default Details;
