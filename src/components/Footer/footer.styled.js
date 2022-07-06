6e2db845bf86619f5316b7c77c344f54117fdec2
Arnaud <bandonkeyea@gmail.com>
2022-07-06 10:06:58 -0600

    Style footer using styled-components

---
+++ src/components/Footer/footer.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const End = styled.footer`
+  padding: 1rem 0;
+  background-color: black;
+  text-align: center;
+  color: white;
+`;
+
+export const FooterLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`;
