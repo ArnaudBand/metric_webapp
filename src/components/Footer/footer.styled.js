63bcd7681e0fe6d1218772a97e3da421fdda5a1c
Arnaud <bandonkeyea@gmail.com>
2022-07-07 09:35:32 -0600

    Change background color

--- src/components/Footer/footer.styled.js
+++ src/components/Footer/footer.styled.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const End = styled.footer`
   padding: 1rem 0;
-  background-color: black;
+  background-color: #548ce8;
   text-align: center;
   color: white;
 `;
