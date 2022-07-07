58d862fe5de9a3faafa6734d4a51522960106add
Arnaud <bandonkeyea@gmail.com>
2022-07-07 09:27:26 -0600

    Remove map from Countries component

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useSelector } from 'react-redux/es/exports';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import {
-  MapContainer, TileLayer, Marker, Popup,
-} from 'react-leaflet';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './countries.css';
@@ -12,7 +9,6 @@ const Countries = () => {
   const countries = useSelector((state) => state.reduceData);
 
   const regionCountry = countries.filter((country) => country.continent === 'Europe');
-
   const container = regionCountry.map((country) => (
     <Link to={`/Details/${country.name}`} key={country.name} className="container_country">
       <img src={country.flag} alt={country.name} width={100} className="country_flag" />
@@ -46,42 +42,9 @@ const Countries = () => {
   return (
     <div className="container_app">
       <Header />
-      <div className="map">
-        <MapContainer center={[51.505, -0.09]} zoom={4} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {regionCountry.map((country, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <Marker key={index} position={[country.lat, country.long]}>
-              <Popup>
-                <div className="div_flex">
-                  <h3 className="titl_c">
-                    {country.lt}
-                  </h3>
-                  <img src={country.flag} alt={country.name} width={80} className="country_flag" />
-                </div>
-                <p>
-                  Population:
-                  {' '}
-                  {country.population}
-                </p>
-                <p>
-                  Cases:
-                  {' '}
-                  {country.cases}
-                </p>
-                <p>
-                  Death:
-
-                  {country.death}
-                </p>
-              </Popup>
-            </Marker>
-          ))}
-
-        </MapContainer>
+      <div className="div_header">
+        <img src="europeCountries.png" alt="Europe" className="headerImageSc" />
+        <img src="covidGif.gif" alt="Europe" className="headerImageRes" />
       </div>
       <div className="input_div">
         <input type="text" id="input" placeholder="Type a country name" onChange={handleChange} />
