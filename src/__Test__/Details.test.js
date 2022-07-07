34ad0899c6d85c08961fd98b3ac60b0dd83b3393
Arnaud <bandonkeyea@gmail.com>
2022-07-07 09:44:46 -0600

    Correct Linter errors on testing

--- src/__Test__/Details.test.js
+++ src/__Test__/Details.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from '../Redux/configureStore';
+import store from '../redux/configureStore';
 import Details from '../components/Details/Details';
 
 describe('Render components', () => {
